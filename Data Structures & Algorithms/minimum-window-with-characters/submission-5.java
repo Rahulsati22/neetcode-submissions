bb90e60a09a94a8d5b891b777e1f5b7c7acdfe0e
Rahulsati22 <97585225+Rahulsati22@users.noreply.github.com>
2026-07-27 10:31:06 +0530

    Add: minimum-window-with-characters - submission-5

---
+++ Data Structures & Algorithms/minimum-window-with-characters/submission-5.java
@@ -0,0 +1,45 @@
+class Solution {
+
+    //minor change here, in previous we had to find exact
+    public boolean isEqual(int arrS[], int arrT[], int len){
+        int cnt = 0;
+        for (int i = 0; i < 300; i++){
+            if (arrT[i] != 0 && arrS[i] < arrT[i])
+                return false;
+        }
+        return true;
+    }
+    public String minWindow(String s, String t) {
+       
+
+        if (t.length() > s.length())
+            return "";
+
+        int arrS[] = new int[300];
+        int arrT[] = new int[300];
+
+        for (int i = 0; i < t.length(); i++){
+            arrT[(int)t.charAt(i)]++;
+        }
+
+        int left = 0, len = Integer.MAX_VALUE;
+
+        int ansLeft = 0, ansRight = 0;
+        for (int r = 0; r < s.length(); r++){
+            arrS[(int)s.charAt(r)]++;
+            while (isEqual(arrS, arrT, t.length())){
+                if (r - left + 1 < len){
+                    ansLeft = left;
+                    ansRight = r;
+                    len = r - left + 1;
+                }
+                arrS[(int)s.charAt(left)]--;
+                left++;
+            }
+        }
+
+        if (len == Integer.MAX_VALUE)
+            return "";
+        return s.substring(ansLeft, ansRight+1);
+    }
+}
