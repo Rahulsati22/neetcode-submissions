9165c0ab333520c408a0b50213730fb29450a7a8
Rahulsati22 <97585225+Rahulsati22@users.noreply.github.com>
2026-07-27 09:11:16 +0530

    Add: permutation-string - submission-1

---
+++ Data Structures & Algorithms/permutation-string/submission-1.java
@@ -0,0 +1,26 @@
+class Solution {
+    public boolean isEqual(String s1, String s2){
+        int arr[] = new int[26];
+        for (char i : s1.toCharArray())
+            arr[i-'a']++;
+        
+        for (char i : s2.toCharArray())
+            arr[i-'a']--;
+        
+        for (int i : arr)
+            if (i != 0)
+                return false;
+        return true;
+    }
+    public boolean checkInclusion(String s1, String s2) {
+        //isko hum 
+
+        ArrayList<String> list = new ArrayList<>();
+        for (int i = 0; i <= s2.length()-s1.length(); i++){
+            String s = s2.substring(i, i + s1.length());
+            if (isEqual(s, s1))
+                return true;
+        }
+        return false;
+    }
+}
