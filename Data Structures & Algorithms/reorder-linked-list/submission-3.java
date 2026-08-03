5b25e7161d3b5df0dc79c7b9adcc250f33bbe6b8
Rahulsati22 <97585225+Rahulsati22@users.noreply.github.com>
2026-08-03 20:36:33 +0530

    Add: reorder-linked-list - submission-3

---
+++ Data Structures & Algorithms/reorder-linked-list/submission-3.java
@@ -0,0 +1,59 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+
+class Solution {
+    public ListNode findMid(ListNode node){
+        if (node == null || node.next == null)
+            return node;
+        ListNode slow = node, fast = node, prev = slow;
+        while (fast != null && fast.next != null){
+            prev = slow;
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        ListNode helper = slow.next;
+        slow.next = null;
+        return helper;
+    }
+    public ListNode reverse(ListNode head) {
+        if (head == null || head.next == null)
+            return head;
+        
+        ListNode start = head, prev = null, next = head.next;
+        while (start != null){
+            start.next = prev;
+            prev = start;
+            start = next;
+            if (next != null)
+                next = next.next;
+        }
+        return prev;
+    }
+    public void reorderList(ListNode head) {
+        if (head == null || head.next == null)return;
+        //prev.next = null, return slow
+        ListNode secondHalf = findMid(head);
+        ListNode rSH = reverse(secondHalf);
+        // System.out.println(head.val + " " + rSH.val);
+
+
+        while (head != null && rSH != null){
+            ListNode hNext = head.next;
+            ListNode rNext = rSH.next;
+
+            head.next = rSH;
+            rSH.next = hNext;
+
+            head = hNext;
+            rSH = rNext;
+        }
+    }
+}
